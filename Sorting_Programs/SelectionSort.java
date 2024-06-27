babe0b8bd33fbead6b647201d3396a7267670152
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-06-28 00:01:29 +0530

    ok

--- Sorting_Programs/SelectionSort.java
+++ Sorting_Programs/SelectionSort.java
@@ -4,6 +4,20 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int arr[]= {8,9,7,6,5,4};
+        int swap;
+
+        for(int i = 1; i<arr.length-1;i++)
+        {
+            int temp = arr[i];
+            for( int j = i-1 ; j>=0 ; j--){
+                if(arr[j]>temp){
+                    swap=temp;
+                    temp=arr[j];
+                    arr[j]=swap;
+
+                }
+            }
+        }
        
     }
     
