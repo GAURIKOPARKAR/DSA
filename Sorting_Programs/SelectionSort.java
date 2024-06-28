6237f3425b13f111be811489a1dd75fd6779c9b8
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-06-29 00:58:44 +0530

    ok in syso

--- Sorting_Programs/SelectionSort.java
+++ Sorting_Programs/SelectionSort.java
@@ -3,6 +3,18 @@ package Sorting_Programs;
 public class SelectionSort {
 
     public static void main(String[] args) {
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
     
     }
     
