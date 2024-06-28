dddba7f0f77c38331543942f5b836d6ea6513ed3
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-06-29 00:35:30 +0530

    changed

--- Sorting_Programs/SelectionSort.java
+++ Sorting_Programs/SelectionSort.java
@@ -3,15 +3,7 @@ package Sorting_Programs;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int arr[]= {8,9,7,6,5,4};
-        int swap;
-
-        for(int i = 1; i<arr.length-1;i++)
-        {
-           
-            }
-      
-       
+    
     }
     
 }
